b1ad619782760e7a08c1cfee306830413a5fe050
anhkhoi1511123 <kanhkhoi32@gmail.com>
2026-03-10 21:54:45 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1077,7 +1077,7 @@ function renderUserArea() {
       <button class="user-menu-toggle" type="button" id="userMenuToggle">
   <span class="dot"></span>
   <span>${escapeHtml(currentUser.displayName || currentUser.username)}</span>
-  <span class="user-menu-caret">▼</span>
+  <span class="user-menu-caret">▾</span>
 </button>
 
       <div class="user-menu-panel" id="userMenuPanel">
